71a70ab3ae9aa5a54fa9deb0343e16692941f585
Scott Faries <tsfaries@gmail.cmm>
2022-09-15 20:44:13 +0000

    add map class with validate/calc position methods

--- src/levelup/map.py
+++ src/levelup/map.py
@@ -1,22 +1,41 @@
-
+from levelup.controller import Direction
 # map class
 
 class Map:
 
     def __init__(self):
-        self.status = GameStatus()
+        self.min_x = 1
+        self.max_x = 10
+        self.min_y = 1
+        self.max_y = 10
 
-    def create_character(self, character_name: str) -> None:
-        if not character_name:
-            character_name = DEFAULT_CHARACTER_NAME
-        self.status.character = Character(character_name)
+    def validatePosition(self, current_position: tuple) -> bool:
+        x_coord = current_position[0]
+        y_coord = current_position[1]
+        
+        if (x_coord >= self.min_x and x_coord <= self.max_x):
+            if (y_coord >= self.min_y and y_coord <= self.max_y):
+                return True
+            else:
+                return False
+        else:
+            return False
 
-    def move(self, direction: Direction) -> None:
-        print(f"Moved {direction.name}")
-        self.character.move(direction)
+    def calculatePosition(self, current_position: tuple, direction: str) -> tuple:
+        x_coord = current_position[0]
+        y_coord = current_position[1]
 
-    def getStatus():
-        pass
-
-    def startGame():
-        pass
+        if direction.lower() in ('north','n'):
+            if y_coord < self.max_y:
+                y_coord += 1
+        elif direction.lower() in ('south','s'):
+            if y_coord > self.min_y:
+                y_coord -= 1
+        elif direction.lower() in ('east','e'):
+            if x_coord < self.max_x:
+                x_coord += 1
+        elif direction.lower() in ('west','w'):
+            if x_coord > self.min_x:
+                x_coord -= 1
+                
+        return((x_coord, y_coord))
